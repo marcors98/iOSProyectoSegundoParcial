933cf24b2fc99d60d3b35489f6abbbeb7bc6bc49
Marco Antonio <ulsa@Jenny.local>
2019-10-08 14:44:57 -0700

    inicio

---
+++ ProyectoParcial/AgregarAuto.swift
@@ -0,0 +1,24 @@
+//
+//  AgregarAuto.swift
+//  ProyectoParcial
+//
+//  Created by Alumno on 10/8/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class AgregarAuto: UIViewController {
+
+    @IBAction func act_Back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
---
+++ ProyectoParcial/AgregarAutoController.swift
@@ -0,0 +1,9 @@
+//
+//  AgregarAutoController.swift
+//  ProyectoParcial
+//
+//  Created by Alumno on 10/8/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
