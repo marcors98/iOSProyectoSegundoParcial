cb096dbb245336bdaed00d149f9eef6aa062d44d
Marco <ulsa@iMacD1Maestro.local>
2019-10-09 15:51:18 -0700

    Errores a lo wey

---
+++ ProyectoParcial/Cell_ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Cell_ViewController.swift
+//  ProyectoParcial
+//
+//  Created by Alumno on 10/9/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class Cell_ViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+
+   
+
+}
--- ProyectoParcial/ViewController.swift
+++ ProyectoParcial/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tb_Carros: UITableViewCell!
+    
+    var Lista : [Modelo_Autos] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tb_Carros.dataSource = self
+        tb_Carros.deledate = self
+        Lista = createArray()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Lista.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celda") as! Cell_ViewController
+        
+        let list = Lista[indexPath.row]
+        
+        celda.set(Modelo : list)
+        
+        return celda
+    }
+    
 }
 
